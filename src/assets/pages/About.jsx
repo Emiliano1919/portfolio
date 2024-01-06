dcd128cb72f89ee79de2a72907b3736201e091ec
Emiliano1919 <emilianojg38@gmail.com>
2024-01-06 12:27:20 -0500

    Fix typo

--- src/assets/pages/About.jsx
+++ src/assets/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
         self-motivated web developer and data scientist based in Ottawa.</p>
         <p> I'm proficient with React, Python, Java, JavaScript, and PostgreSQL.</p>
         <p>I've used my creativity and problem-solving skills to discover trends,
-           implement and design dashboards and websites, and improve efficiency.
+           implement and design dashboards/websites, and improve efficiency.
         </p>
       </div>
     )
