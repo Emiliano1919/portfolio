f6b78893252b585f82eaac458364aae819e9532c
Emiliano1919 <emilianojg38@gmail.com>
2024-01-06 13:26:08 -0500

    Add content

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -1,7 +1,16 @@
-
+import './Contact.css'
 function Contact() {
     return(
-      <div style={{color:'white'}}>Contact Page</div>
+      <div className="container">
+        <div className="question">
+          <h2>WANT TO GET IN TOUCH?</h2>
+          <p>emilianojg38@gmail.com</p>
+        </div>
+        <div className="links">
+          <a href="https://github.com/Emiliano1919" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://www.linkedin.com/in/emiliano-jimenez-guadarrama-2789912a6/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </div>
+      </div>
     )
   }
   
