d9a7947af9253e7ae3dcb4186f55109e0dff8ad9
Emiliano1919 <emilianojg38@gmail.com>
2024-01-07 12:55:11 -0500

    Improve size for mobile devices

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -4,20 +4,22 @@ import GenerativeArt from './GenerativeArt.jsx';
 function HomePage() {
     return (
         <>
-        <div className="your-component">
-            <GenerativeArt/>
-        </div>
-        <div className="overlay-text">
-                <div className='first'>
-                    <h1 >Emiliano</h1>
-                </div>
-                <div className='second'>
-                    <h1>Jimenez</h1>
-                </div>
-                <div className='third'>
-                    <h1>Guadarrama</h1>
-                </div>
+        <div className="|">
+            <div className="your-component">
+                <GenerativeArt/>
             </div>
+            <div className="overlay-text">
+                    <div className='first'>
+                        <h1 >Emiliano</h1>
+                    </div>
+                    <div className='second'>
+                        <h1>Jimenez</h1>
+                    </div>
+                    <div className='third'>
+                        <h1>Guadarrama</h1>
+                    </div>
+            </div>
+        </div>
         </>
         
       )
