3e8dd69afbccddff982a48f424cb08ebdc8b85bd
Emiliano1919 <emilianojg38@gmail.com>
2024-01-07 12:22:26 -0500

    Add text to projects

--- src/assets/pages/Projects.jsx
+++ src/assets/pages/Projects.jsx
@@ -1,7 +1,17 @@
-
+import './About.css'
 function Projects() {
     return(
-      <div style={{color:'white'}}>Projects Page</div>
+      <div className='introduction'>
+        <ul>
+          <li>On the sreets of Ottawa with Kevin Tawadros and Alexis Taillardat</li>
+          <li>On the sreets of Ottawa (Dashboard)</li>
+          <li>Who do I bet on Insights from machine learning into French Trot horse racing</li>
+          <li>Weather web app</li>
+          <li>Solutions for the book: How to prove it by Velleman</li>
+          <li>Solutions for the book: Introduction to probability by Blitzstein and Hwang</li>
+          <li>This portfolio :)</li>
+        </ul>
+      </div>
     )
   }
   
