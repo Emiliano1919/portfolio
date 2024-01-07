d9de2d786d9ff7ac41f31cccf497a24f8bbbce29
Emiliano1919 <emilianojg38@gmail.com>
2024-01-07 12:48:51 -0500

    Improve size for mobile devices

--- src/assets/pages/Projects.jsx
+++ src/assets/pages/Projects.jsx
@@ -1,24 +1,27 @@
 import './About.css'
 function Projects() {
     return(
-      <div className='introduction'>
-        <ul>
-          <li><a href="https://github.com/kvn154/On-the-streets-of-Ottawa">
-            On the sreets of Ottawa with Kevin Tawadros and Alexis Taillardat</a></li>
-          <li><a href="on-the-streets-of-ottawa-dashboard.onrender.com">
-            On the sreets of Ottawa (Dashboard)</a></li>
-          <li><a href="https://github.com/Emiliano1919/Who-do-I-bet-on-Insights-from-machine-learning-into-French-Trot-horse-racing">
-            Who do I bet on Insights from machine learning into French Trot horse racing</a></li>
-          <li><a href="https://emiliano1919.github.io/Weather-WebApp/">
-            Weather web app</a></li>
-          <li><a href="https://github.com/Emiliano1919/Introduction-to-probability">
-            Solutions for the book: Introduction to probability by Blitzstein and Hwang</a></li>
-          <li><a href="https://github.com/Emiliano1919/Velleman_Solutions">
-            Solutions for the book: How to prove it by Velleman</a></li>
-          <li><a href="https://github.com/Emiliano1919/portfolio">
-            This portfolio :)</a></li>
-        </ul>
-      </div>
+      <>
+      <div className="snow"></div>
+        <div className='introduction'>
+          <ul>
+            <li><a href="https://github.com/kvn154/On-the-streets-of-Ottawa">
+              On the sreets of Ottawa with Kevin Tawadros and Alexis Taillardat</a></li>
+            <li><a href="on-the-streets-of-ottawa-dashboard.onrender.com">
+              On the sreets of Ottawa (Dashboard)</a></li>
+            <li><a href="https://github.com/Emiliano1919/Who-do-I-bet-on-Insights-from-machine-learning-into-French-Trot-horse-racing">
+              Who do I bet on Insights from machine learning into French Trot horse racing</a></li>
+            <li><a href="https://emiliano1919.github.io/Weather-WebApp/">
+              Weather web app</a></li>
+            <li><a href="https://github.com/Emiliano1919/Introduction-to-probability">
+              Solutions for the book: Introduction to probability by Blitzstein and Hwang</a></li>
+            <li><a href="https://github.com/Emiliano1919/Velleman_Solutions">
+              Solutions for the book: How to prove it by Velleman</a></li>
+            <li><a href="https://github.com/Emiliano1919/portfolio">
+              This portfolio :)</a></li>
+          </ul>
+        </div>
+      </>
     )
   }
   
