ccb1023e4456c3f00701676a740ae764443b5c20
Emiliano1919 <emilianojg38@gmail.com>
2024-03-14 14:47:05 -0400

    Fix Links

--- src/assets/pages/Projects.jsx
+++ src/assets/pages/Projects.jsx
@@ -5,21 +5,21 @@ function Projects() {
       <div className="snow"></div>
         <div className='introduction'>
           <ul>
-            <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+            <li><a href="https://github.com/kvn154/On-the-streets-of-Ottawa" target="_blank" rel="noopener noreferrer">
               On the streets of Ottawa with Kevin Tawadros and Alexis Taillardat</a></li>
-            <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+            <li><a href="https://github.com/Emiliano1919/On_the_streets_of_Ottawa_Dashboard" target="_blank" rel="noopener noreferrer">
               On the streets of Ottawa (Dashboard)</a></li>
             <li><a href="https://github.com/Emiliano1919/generative-node" target="_blank" rel="noopener noreferrer">
               Website for Generative art with NodeJS</a></li>
-            <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+            <li><a href="https://github.com/Emiliano1919/Who-do-I-bet-on-Insights-from-machine-learning-into-French-Trot-horse-racing" target="_blank" rel="noopener noreferrer">
               Who do I bet on Insights from machine learning into French Trot horse racing</a></li>
-            <li><a href="" target="_blank" rel="noopener noreferrer">
+            <li><a href="https://github.com/Emiliano1919/Weather-WebApp" target="_blank" rel="noopener noreferrer">
               Weather web app</a></li>
-            <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+            <li><a href="https://github.com/Emiliano1919/Introduction-to-probability" target="_blank" rel="noopener noreferrer">
               Solutions for the book: Introduction to probability by Blitzstein and Hwang</a></li>
-            <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+            <li><a href="https://github.com/Emiliano1919/Velleman_Solutions" target="_blank" rel="noopener noreferrer">
               Solutions for the book: How to prove it by Velleman</a></li>
-            <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+            <li><a href="https://github.com/Emiliano1919/portfolio" target="_blank" rel="noopener noreferrer">
               This portfolio :)</a></li>
           </ul>
         </div>
