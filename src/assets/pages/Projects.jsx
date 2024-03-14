b14cfd15412fdb6745d0c5dd8fd87c9767f6250c
Emiliano1919 <emilianojg38@gmail.com>
2024-03-14 14:28:47 -0400

    Add new project

--- src/assets/pages/Projects.jsx
+++ src/assets/pages/Projects.jsx
@@ -9,6 +9,8 @@ function Projects() {
               On the streets of Ottawa with Kevin Tawadros and Alexis Taillardat</a></li>
             <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">
               On the streets of Ottawa (Dashboard)</a></li>
+            <li><a href="https://github.com/Emiliano1919/generative-node" target="_blank" rel="noopener noreferrer">
+              Website for Generative art with NodeJS</a></li>
             <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">
               Who do I bet on Insights from machine learning into French Trot horse racing</a></li>
             <li><a href="" target="_blank" rel="noopener noreferrer">
